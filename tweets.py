2c32c428f713b5f0bc1e5fa92b224c7d36ae2a73
YousufNofal0 <YousufNofal2@gmail.com>
2022-05-06 14:05:50 +0200

    Data cleansing is done & regex cheat sheet is uploaded.

---
+++ tweets.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import re
+
+url = r'Tweets\bbchealth.txt'
+
+df = pd.read_table(url, names = ['Tweet'])
+
+df = df['Tweet'].str.split('|', expand=True)
+
+df = df[[2]]
+match = re.compile(r'http:\S+')
+df[2] = df[2].str.replace(match, '', regex = True)
+
+match = re.compile(r'@\S+')
+df[2] = df[2].str.replace(match, '', regex = True)
+
+match = re.compile(r'#')
+df[2] = df[2].str.replace(match, '', regex = True)
+
+df[2] = df[2].str.lower()
+
+print(df.head())
+print(df.describe())
+
